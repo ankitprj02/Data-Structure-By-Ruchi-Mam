0d522cf83095c698d1b3a8e60bf4ce2b2e4adc6d
Ankit Prajapati <77101852+ankitprj02@users.noreply.github.com>
2024-09-22 11:57:55 +0530

    Questions

---
+++ 17-Sep-2024/questions.c
@@ -0,0 +1,18 @@
+'''
+  1. Find the largest number among the three numbers entered by the user.
+
+2. Check whether an alphabet entered by the user is a vowel or a consonant.
+
+3. Check whether the year entered by the user is a leap year or not.
+
+4. Write a C program to check whether a triangle is Equilateral, Isosceles or Scalene.
+
+5. Write a C program to read temperature in centigrade and display a suitable message according to temperature state below : 
+  Go to the editor
+  Temp < 0 then Freezing weather
+  Temp 0-10 then Very Cold weather
+  Temp 10-20 then Cold weather
+  Temp 20-30 then Normal in Temp
+  Temp 30-40 then Its Hot
+  Temp >=40 then Its Very Hot
+'''
